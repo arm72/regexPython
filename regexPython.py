5da652d3f77e9bef29b24e80c363de83476548f1
arm72 <36706874+arm72@users.noreply.github.com>
2022-01-29 20:06:37 -0800

    Add files via upload

---
+++ regexPython.py
@@ -0,0 +1,20 @@
+﻿#!/usr/bin/env python
+#!python
+
+
+
+import re
+from sys import *
+
+
+file = open("regex.txt","r")
+text = file.read()
+regex = r".+(?=View)"
+
+
+for search in re.finditer(regex, text):
+    print search.group()
+
+
+
+exit()
